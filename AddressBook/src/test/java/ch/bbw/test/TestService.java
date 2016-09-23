cb93e21857aa9237008e4dfc08106f2a493467c6
5ia14luspuehler <5ia14luspuehler@INFP21-40.gibwintra.ch>
2016-09-23 11:00:44 +0200

    added new test case for function readAllAddresses()

--- AddressBook/src/test/java/ch/bbw/test/TestService.java
+++ AddressBook/src/test/java/ch/bbw/test/TestService.java
@@ -2,9 +2,12 @@ package ch.bbw.test;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import ch.bbw.addressbook.Address;
 import ch.bbw.addressbook.AddressService;
 
 public class TestService {
@@ -17,6 +20,15 @@ public class TestService {
 	public void test1() {
 		AddressService as = new AddressService();
 		assertTrue("Test Service",  as != null);
+	}
+	
+	@Test
+	public void testReadAll() {
+		AddressService as = new AddressService();
+		as.storeAddress(new Address(1, "Luca", "supi", "00011"));
+		
+		List<Address> list = as.getAllAddresses();
+		assertTrue("Test Read All", list.get(1).getFirstname().equals("Luca"));
 	}
 	
 }
