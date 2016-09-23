7cdad0e306b15fc045eee34e99cffa1c85507d89
5ia14luspuehler <5ia14luspuehler@INFP21-40.gibwintra.ch>
2016-09-23 10:44:17 +0200

    unused imports removed

--- AddressBook/src/test/java/ch/bbw/test/TestService.java
+++ AddressBook/src/test/java/ch/bbw/test/TestService.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
-import ch.bbw.addressbook.Address;
 import ch.bbw.addressbook.AddressService;
 
 public class TestService {
